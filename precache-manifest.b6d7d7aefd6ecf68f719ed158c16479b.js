481e5bcd942f68623f3fad70413f0f0254c44262
Hosni Ghribi <ghribi.hosni7@gmail.com>
2019-12-18 09:28:46 +0100

    Updates

---
+++ precache-manifest.b6d7d7aefd6ecf68f719ed158c16479b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca4a470f5e514c4c4a8d17f78736e84",
+    "url": "/.github.iomovie-app-react-redux/index.html"
+  },
+  {
+    "revision": "a5953f2470b814e4c0d5",
+    "url": "/.github.iomovie-app-react-redux/static/css/main.3ba39aa0.chunk.css"
+  },
+  {
+    "revision": "f2819eb5429168d44699",
+    "url": "/.github.iomovie-app-react-redux/static/js/2.e4ce1e8e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/.github.iomovie-app-react-redux/static/js/2.e4ce1e8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5953f2470b814e4c0d5",
+    "url": "/.github.iomovie-app-react-redux/static/js/main.36f91d6f.chunk.js"
+  },
+  {
+    "revision": "862ebbd7dd4dd7d0774d",
+    "url": "/.github.iomovie-app-react-redux/static/js/runtime-main.c44d5726.js"
+  }
+]);
